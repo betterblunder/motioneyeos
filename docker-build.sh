eed826688e3912722979c6bc681e7b1e2534f256
betterblunder <03-better-blunder@icloud.com>
2023-07-30 03:01:37 -0700

    try adding ci

--- docker-build.sh
+++ docker-build.sh
@@ -22,7 +22,7 @@ if [ "${SHOW_USAGE}" = "true" ]; then
 fi
 
 if [ "${CONTAINER_NAME}" = "" ]; then
-  CONTAINER_NAME="${IMAGE_NAME}-${RANDOM}"
+   docker run --rm --user=root --mount type=bind,src=${PWD},target=/build "${IMAGE_NAME}" ./build.sh "$@"
+ else
+   docker run --rm --user=root --name "${CONTAINER_NAME}" --mount type=bind,src=${PWD},target=/build "${IMAGE_NAME}" ./build.sh "$@"
 fi
-
-docker run --rm --name "${CONTAINER_NAME}" --mount type=bind,src=${PWD},target=/build -it "${IMAGE_NAME}" ./build.sh "$@"
